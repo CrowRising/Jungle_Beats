0b6d677b7015e617da3e492ec999819811d782d1
Crow Rising <miscbyrd@gmail.com>
2023-02-02 20:14:32 -0700

    Add first two tests

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,1 +1,22 @@
-class Node
+require './lib/node'
+
+RSpec.describe Node do
+  describe "#instance" do
+    it 'is an instance of Node' do
+    
+    node = Node.new("plop")
+    
+    expect(node).to be_instance_of(Node)
+    end
+  end
+
+  describe "#data" do
+    it 'can return data' do
+
+    node = Node.new("plop")
+     
+    expect(node.data).to eq("plop")
+    end
+  end
+end
+  
