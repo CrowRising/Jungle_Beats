5e41328ff424404ead6ed0fb1b6866951a01643b
Crow Rising <miscbyrd@gmail.com>
2023-02-04 13:30:14 -0700

    Finish iteration 1

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -26,10 +26,10 @@ RSpec.describe LinkedList do
     it 'adds data to head' do
       list = LinkedList.new
       list.append("doop")
-      # require 'pry'; binding.pry
-
-      expect(list.head.data).to eq("doop")#important to note not expecting .append, expecting that the command of append is reaching into the list into the node(head) and grabbing the data
+      # list.append("deep") # I am trying to see if I can combine these commands into one test
+      #It did NOT work
       
+      expect(list.head.data).to eq("doop") #important to note not expecting .append, expecting that the command of append is reaching into the list into the node(head) and grabbing the data
     end
   end
 
@@ -38,8 +38,26 @@ RSpec.describe LinkedList do
 
     list = LinkedList.new
     list.append("doop")
-    # list.head.next_node
-# require 'pry'; binding.pry
+   
+    expect(list.head.next_node).to eq(nil)
+    end
+  end
+
+  describe "second append" do
+    it "adds data to next node" do
+      list = LinkedList.new
+      list.append("deep")
+
+      expect(list.head.data).to eq("deep")
+    end
+  end
+
+  describe "#next_node" do
+    it "checks the value of the next node" do
+
+    list = LinkedList.new
+    list.append("deep")
+   
     expect(list.head.next_node).to eq(nil)
     end
   end
@@ -48,9 +66,10 @@ RSpec.describe LinkedList do
     it "counts the items in the list" do
       list = LinkedList.new
       list.append("doop")
+      list.append("deep")
       list.count
 # require 'pry'; binding.pry
-      expect(list.count).to eq(1)
+      expect(list.count).to eq(2)
     end
     
   end
@@ -59,9 +78,10 @@ RSpec.describe LinkedList do
     it "calls a string" do
       list = LinkedList.new
       list.append("doop")
-      # list.to_string
+      list.append("deep")
+      list.to_string
 # require 'pry'; binding.pry
-      expect(list.head.data).to eq("doop") #this is such an important step to reach thru at it were into the list into the node and grab the data
+      expect(list.to_string).to eq("doop deep") #this is such an important step to reach thru at it were into the list into the node and grab the data
     end
 
   end
