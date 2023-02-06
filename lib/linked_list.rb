8dc74063bc6e65f78af54bea128c7cc90d17c451
Crow Rising <miscbyrd@gmail.com>
2023-02-05 17:34:08 -0700

    Work on adding additional methods to LinkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -20,61 +20,81 @@ class LinkedList
     end
   end
 
-    def count
-      count = 1
-      if @head.nil?
-        count = 0
-      else
-        current_node = @head
+  def count
+    count = 1
+    if @head.nil?
+      count = 0
+    else
+      current_node = @head
 
-        while(!current_node.next_node.nil?)
-          count += 1
-          current_node = current_node.next_node
-      end
-       count
-       end
-     
+      while(!current_node.next_node.nil?)
+        count += 1
+        current_node = current_node.next_node
     end
-    def to_string
+    count
+    end
+    
+  end
+
+  def to_string
+    to_string = []
+    if @head.nil?
       to_string = []
-      if @head.nil?
-        to_string = []
-      else 
-        current_node = @head
+    else 
+      current_node = @head
 
-        while(!current_node.nil?)
-          to_string << current_node.data
-          current_node = current_node.next_node
+      while(!current_node.nil?)
+        to_string << current_node.data
+        current_node = current_node.next_node
+    end
+    to_string.join(" ")
+    end
+  end
+
+  def prepend(data)
+    if @head.nil?
+      @head = Node.new(data)
+    else
+      current_head = Node.new(data)
+      current_head.next_node = @head
+      @head = current_head
+
+    end
+  end
+
+  def insert(index, data)
+    node = Node.new(data)
+    if @head.nil?
+      @head = node
+    else
+      current_node = @head
+      (index - 1).times do 
+        current_node = current_node.next_node
+    end
+      node.next_node = current_node.next_node
+      current_node.next_node = node
+    end
+  end
+
+  def find(index, amount)
+    current_node = @head
+    string = []
+    (index).times do
+      current_node = current_node.next_node
+    end
+    (amount).times do
+      string << current_node.data
+      current_node.next_node
+    end
+    string.join(" ")
+  end
+
+  def includes?(data)
+    current_node = @head
+    until current_node.data == data || current.next_node == nil
+      current_node = current_node.next_node
+    end
+    current_node.data == (data)
+  end
   
-      end
-      to_string.join(" ")
-    # require 'pry'; binding.pry
-      end
-    end
-
-    def prepend(data)
-      if @head.nil?
-        @head = Node.new(data)
-      else
-        current_head = Node.new(data)
-        current_head.next_node = @head
-        @head = current_head
-
-      end
-    end
-
-    def insert(index, data)
-      node = Node.new(data)
-      if @head.nil?
-        @head = node
-      else
-        require 'pry'; binding.pry
-        current_node = @head
-        (index - 1).times do 
-          current_node = current_node.next_node
-        end
-        node.next_node = current_node.next_node
-        current_node.next_node = node
-      end
-    end
 end
